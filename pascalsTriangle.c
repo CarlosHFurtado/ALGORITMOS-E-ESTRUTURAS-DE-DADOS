b06de1804fe54a33c1f853b84f0bc30d2ed5b962
Carlos Henrique Goebel Teixeira Furtado <143563901+CarlosHFurtado@users.noreply.github.com>
2024-12-15 21:54:48 -0300

    Update pascalsTriangle.c

--- pascalsTriangle.c
+++ pascalsTriangle.c
@@ -11,22 +11,22 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 * The sizes of the arrays are returned as *returnColumnSizes array.
 * Note: Both returned array and *columnSizes array must be malloced assume caller calls free().
 */
-int** generate(int numRows, int* returnSize, int** returnColumnSizes){
+int** generate( int numRows, int* returnSize, int** returnColumnSizes ){
     *returnSize = numRows;
-    *returnColumnSizes = malloc(sizeof(int) * numRows);
+    *returnColumnSizes = malloc ( sizeof ( int ) * numRows );
 
-    int **x = malloc(sizeof(int*) * numRows);
+    int **x = malloc ( sizeof ( int* ) * numRows );
 
-    for (int i = 0; i < numRows; i++) {
-        (*returnColumnSizes)[i] = i + 1;
-        x[i] = malloc(sizeof(int) * (i + 1));
+    for ( int i = 0; i < numRows; i++ ) {
+        ( *returnColumnSizes )[i] = i + 1;
+        x[i] = malloc ( sizeof ( int ) * ( i + 1 ) );
         x[i][0] = 1;
         x[i][i] = 1;
 
         }
                                                                             
-    for (int i = 2; i < numRows; i++) {
-        for (int j = 1; j < i; j++) {
+    for ( int i = 2; i < numRows; i++ ) {
+        for ( int j = 1; j < i; j++ ) {
             x[i][j] = x[i - 1][j - 1] + x[i - 1][j];
 
         }
